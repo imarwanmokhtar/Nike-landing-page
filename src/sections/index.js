121a26c5089cddd28162f080d6f1645fb683d190
imarwanmokhtar <marwanmokhtar000@gmail.com>
2024-06-20 19:31:17 +0200

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality'
+
+export {
+    Services, 
+    SpecialOffer, 
+    CustomerReviews, 
+    Subscribe, 
+    Footer, 
+    Hero, 
+    PopularProducts, 
+    SuperQuality
+};
